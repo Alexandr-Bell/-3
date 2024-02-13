6e2f83f909026298d38c96c11a24e8fd4d9e88fa
Александр <121234051+Alexandr-Bell@users.noreply.github.com>
2024-02-13 20:03:04 +0300

    Update lesson_3_task_1.py

--- lesson_3_task_1.py
+++ lesson_3_task_1.py
@@ -1,4 +1,6 @@
 from user import User
 
-
-my_user = (User)
+User1 = User("Александр","Иванов")
+print(User1.first_name)
+print(User1.last_name)
+print("Меня зовут", User1.first_name, User1.last_name)
