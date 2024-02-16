6489c0d482cd2fb75370b53584c3c7d7ab9f3e42
Александр <121234051+Alexandr-Bell@users.noreply.github.com>
2024-02-16 18:58:05 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,13 +1,13 @@
-class User():
-  
-    def  __init__(self, first_name, last_name):
+class User:
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
 
-    def name(self):
-        print( self.first_name + self.last_name)
-User1 = User("Александр","Иванов")
+    def print_first_name(self):
+        print(f"First Name: {self.first_name}")
 
-print(User1.first_name)
-print(User1.last_name)
-print("Меня зовут", User1.first_name, User1.last_name)
+    def print_last_name(self):
+        print(f"Last Name: {self.last_name}")
+
+    def print_full_name(self):
+        print(f"Full Name: {self.first_name} {self.last_name}")
