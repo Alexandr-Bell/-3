957ea4550914a4841b1c27da18932ee507f5fb16
Александр <121234051+Alexandr-Bell@users.noreply.github.com>
2024-02-21 21:44:57 +0300

    Update lesson_3_task_4.py

--- lesson_3_task_4.py
+++ lesson_3_task_4.py
@@ -1,31 +1,71 @@
 import turtle
-# Рисунок мыши "примитивно"
+ 
+
+ 
+
 t = turtle.Turtle()
 
-t.penup()
-t.goto(0, -50)
-t.pendown()
-t.circle(50)
+
+
+t.color('red')
 
 t.penup()
-t.goto(-20, 50)
+t.goto(-100, 90)
 t.pendown()
-t.circle(20)
+t.circle(10)
+
+
 
 t.penup()
-t.goto(20, 50)
-t.pendown()
-t.circle(20)
 
+t.goto(-50, 90)
+t.pendown()
+t.circle(10)
 t.penup()
-t.goto(-25, 60)
+
+t.goto(-80, 0)
+t.pendown()
+t.circle(80)
+t.penup()
+
+t.goto(-110, 60)
 t.pendown()
 t.right(90)
 t.circle(25, 180)
+t.penup()
 
-t.hideturtle()
-turtle.done()
+t.goto(-100, 140)
+t.pendown()
+t.circle(20)
+t.penup()
+
+t.goto(-0, 140)
+t.pendown()
+t.circle(20)
 t.penup()
 
 
-turtle.done()
+t.goto(30, -110)
+t.pendown()
+t.circle(120)
+t.penup()
+
+t.goto(0, -200)
+t.pendown()
+t.circle(10)
+t.penup()
+
+t.goto(-170, -200)
+t.pendown()
+t.circle(10)
+t.penup()
+
+t.goto(-150, 10)
+t.pendown()
+t.circle(25)
+t.penup()
+
+t.goto(18, 10)
+t.pendown()
+t.circle(25)
+t.penup()
