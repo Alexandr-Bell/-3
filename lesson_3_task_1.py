4fd4cc2a63e601a9e1402408d6683cbc7a5ad770
Александр <121234051+Alexandr-Bell@users.noreply.github.com>
2024-02-24 12:19:08 +0300

    Update lesson_3_task_1.py

--- lesson_3_task_1.py
+++ lesson_3_task_1.py
@@ -1,5 +1,8 @@
-my_user = User("Александр", "Иванов")
+from user import User
 
+my_user = User("Алекс", "Иванов")
+
+# методы у объекта my_user
 my_user.print_first_name()
 my_user.print_last_name()
 my_user.print_full_name()
